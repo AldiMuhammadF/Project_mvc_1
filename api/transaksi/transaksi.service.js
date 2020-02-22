9fd759f0e1e9550672a5e4b7102dcdedecc6cd82
Aldi Muhammad Farhan <farhanaldi09@gmail.com>
2020-02-22 18:35:07 +0700

    first

--- api/transaksi/transaksi.service.js
+++ api/transaksi/transaksi.service.js
@@ -1,5 +1,17 @@
-let db = require('../../config/connection');
+let {
+    controllerAddItem,
+    controllerGetItem,
+    controllerUpdateItem,
+    controllerdeleteItem
 
-module.exports = {
+} = require("./barang.controller");
 
-}
+let router = require("express").Router();
+let { checkToken } = require("../../auth/token_validation");
+
+router.post("/", checkToken, controllerAddItem);
+router.get("/", checkToken, controllerGetItem);
+router.patch("/", checkToken, controllerUpdateItem);
+router.delete("/", checkToken, controllerdeleteItem);
+
+module.exports = router;
